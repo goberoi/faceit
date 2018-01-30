104910df873395106b3f0ccaab9ad7bb777094e1
Gaurav Oberoi <goberoi@gmail.com>
2018-01-30 15:27:11 -0800

    Work in progress...

--- faceit.py
+++ faceit.py
@@ -1,52 +1,78 @@
+import numpy
+import cv2
 import os
-from moviepy import VideoFileClip
-from lib.faceswap.lib.faces_detect import detect_faces
-from lib.faceswap.plugins.PluginLoader import PluginLoader
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from foo.faceswap.lib.faces_detect import detect_faces
+from foo.faceswap.plugins.PluginLoader import PluginLoader
 
-class Model
-    BASE_PATH = 'data':
+import sys
+sys.path.append('/home/goberoi/projects/faceit/foo/faceswap')
+
+
+class Model:
+    BASE_PATH = 'data'
     MEDIA_PATH = 'media'
     TRAINING_PATH = 'training'
     OUTPUT_PATH = 'output'
-    
-    def __init__(name, person_a, person_b, media_a, media_b):
+
+    @classmethod
+    def _media_path(cls, filename):
+            return os.path.join(Model.BASE_PATH, Model.MEDIA_PATH, filename)        
+
+    @classmethod        
+    def _training_path(cls, filename):
+            return os.path.join(Model.BASE_PATH, Model.TRAINING_PATH, filename)        
+
+    @classmethod        
+    def _output_path(cls, filename):
+            return os.path.join(Model.BASE_PATH, Model.OUTPUT_PATH, filename)        
+
+    def __init__(self, name, media_a, media_b):
         self._name = name
         self._media_a = media_a
         self._media_b = media_b
 
-    def prepare_data():
-        for media in (self._media_a + self._media_b):
-            if os.path.isfile(media):
-                self._process_video(media)
+    def prepare_data(self):
+        for filename in (self._media_a + self._media_b):
+            if os.path.isfile(self._media_path(filename)):
+                self._process_video(filename)
             else:
-                self._process_images(media)                
+                self._process_images(filename)                
         return
 
-    def _process_video(filename):
-        video_path = os.path.join(BASE_PATH, MEDIA_PATH, filename)
-        training_path = os.path.join(BASE_PATH, TRAINING_PATH, filename)
-        print("Processing video: %s" % video_path)
+    def _process_video(self, filename):
+        print("Processing video: %s" % self._media_path(filename))
         
-        if os.path.exists(training_path):
-            print("Proccessing video already done, found results at: %s" % training_path)
+        if os.path.exists(self._training_path(filename)):
+            print("Proccessing video already done, found results at: %s" % self._training_path(filename))
             return
 
-        output_dir = os.path.join(BASE_PATH, TRAINING_PATH, filename)
-        video = VideoFileClip(video_path)
+        output_dir = self._output_path(filename)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        video = VideoFileClip(self._media_path(filename))
         
         extractor = PluginLoader.get_extractor("Align")()
         frame_num = 0
         for frame in video.iter_frames():
+            print("Processing frame: %03d" % frame_num)
             for (face_num, face) in enumerate(detect_faces(frame)):
+                print("Processing face: %03d" % face_num)                
                 resized_image = extractor.extract(frame, face, 256)
                 output_file = os.path.join(output_dir, 'frame_%03d_%02d.jpg' % (frame_num, face_num))
+                print(output_file)
                 cv2.imwrite(output_file, resized_image)
             frame_num += 1
 
-    def _process_images(media):
+    def _process_images(self, media):
         raise NotImplementedError("directories of images not yet supported")        
 
 if __name__ == '__main__':
-    pass
+    oren_trump = Model('oren_trump', ['oren.mp4'], ['trump_speech.webm'])
+    oren_trump.prepare_data()
+
+#    trump_oren = Model('trump_oren', ['trump_speech.webm'], ['oren.mp4'])
+#    trump_oren.prepare_data()
+
 
 
