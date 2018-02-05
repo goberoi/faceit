976acdbae2ee0ed88621866e63a78ed61b12bfaa
Gaurav Oberoi <goberoi@gmail.com>
2018-02-05 13:32:42 -0800

    Basic working download and extract for youtube videos.

--- faceit.py
+++ faceit.py
@@ -1,79 +1,94 @@
 import sys
 sys.path.append('faceswap')
 
-import numpy
-import cv2
 import os
+from argparse import Namespace
 from moviepy.video.io.VideoFileClip import VideoFileClip
-from lib.faces_detect import detect_faces
-from plugins.PluginLoader import PluginLoader
-
+import youtube_dl
+import cv2
+import time
 
 class Model:
-    BASE_PATH = 'data'
-    MEDIA_PATH = 'media'
-    TRAINING_PATH = 'training'
-    OUTPUT_PATH = 'output'
+    VIDEO_PATH = 'data/videos'
+    
+    def __init__(self, person_a, person_b):
+        self._person_a = person_a
+        self._person_b = person_b
+        self._videos = {
+            self._person_a : [],
+            self._person_b : []
+        }
+        if not os.path.exists(os.path.join(Model.VIDEO_PATH)):
+            os.makedirs(Model.VIDEO_PATH)
 
-    @classmethod
-    def _media_path(cls, filename):
-            return os.path.join(Model.BASE_PATH, Model.MEDIA_PATH, filename)        
+    def add_video(self, person, name, url=None):
+        self._videos[person].append({
+            'name' : name,
+            'url' : url
+        })
 
-    @classmethod        
-    def _training_path(cls, filename):
-            return os.path.join(Model.BASE_PATH, Model.TRAINING_PATH, filename)        
+    def fetch(self):
+        self._process_videos(self._fetch_video)
 
-    @classmethod        
-    def _output_path(cls, filename):
-            return os.path.join(Model.BASE_PATH, Model.OUTPUT_PATH, filename)        
+    def preprocess(self):
+        self._process_videos(self._extract_frames)
 
-    def __init__(self, name, media_a, media_b):
-        self._name = name
-        self._media_a = media_a
-        self._media_b = media_b
+    def _process_videos(self, func):
+        for person, videos in self._videos.items():
+            for video in videos:
+                func(person, video)
 
-    def prepare_data(self):
-        for filename in (self._media_a + self._media_b):
-            if os.path.isfile(self._media_path(filename)):
-                self._process_video(filename)
-            else:
-                self._process_images(filename)                
-        return
+    def _fetch_video(self, person, video):
+        # youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 -o "foobar.%(ext)s" https://www.youtube.com/watch?v=f0UB06v7yLY
+        options = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio',
+#            'outtmpl': os.path.join(Model.VIDEO_PATH, person + '_%(id)s.%(ext)s'),
+            'outtmpl': os.path.join(Model.VIDEO_PATH, video['name']),
+            'merge_output_format' : 'mp4'
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            x = ydl.download([video['url']])
 
-    def _process_video(self, filename):
-        print("Processing video: %s" % self._media_path(filename))
+    def _extract_frames(self, person, video):
+        video_path = os.path.join(Model.VIDEO_PATH, video['name'])
+        video_frames_dir = video_path + '_frames'
+        video = VideoFileClip(video_path)
+
+        start_time = time.time()
+        print('[extract] about to extract_frames for {}, fps {}, length {}s'.format(video_frames_dir, video.fps, video.duration))
         
-        if os.path.exists(self._training_path(filename)):
-            print("Processing video already done, found results at: %s" % self._training_path(filename))
+        if os.path.exists(video_frames_dir):
+            print('[extract] frames already exist, skipping extraction: {}'.format(video_frames_dir))
             return
-
-        training_dir = self._training_path(filename)
-        if not os.path.exists(training_dir):
-            os.makedirs(training_dir)
-            
-        video = VideoFileClip(self._media_path(filename))
         
-        extractor = PluginLoader.get_extractor("Align")()
+        os.makedirs(video_frames_dir)
         frame_num = 0
-        for frame in video.iter_frames():
-            print("Processing frame: %03d" % frame_num)
-            for (face_num, face) in enumerate(detect_faces(frame)):
-                print("Processing face: %03d" % face_num)
-                training_file = os.path.join(training_dir, 'frame%03d_face%02d.jpg' % (frame_num, face_num))
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                resized_image = extractor.extract(frame, face, 256)
-                cv2.imwrite(training_file, resized_image)
+        for frame in video.iter_frames(fps=2):
+            video_frame_file = os.path.join(video_frames_dir, 'frame_%03d.jpg' % (frame_num))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV
+            cv2.imwrite(video_frame_file, frame)
             frame_num += 1
 
-    def _process_images(self, media):
-        raise NotImplementedError("directories of images not yet supported")        
+        print('[extract] finished extract_frames for {}, total frames {}, time taken {:.0f}s'.format(video_frames_dir, frame_num-1, time.time() - start_time))            
 
-if __name__ == '__main__':
-    oren_trump = Model('oren_trump', ['oren.mp4'], ['trump_speech.webm'])
-    oren_trump.prepare_data()
+    def apreprocess(self):
+        args = Namespace(a=1, b='c')
+        pass
 
-#    trump_oren = Model('trump_oren', ['trump_speech.webm'], ['oren.mp4'])
-#    trump_oren.prepare_data()
+model = Model('oren', 'trump')
+
+# Add video data
+model.add_video('trump', 'trump_speech_compilation.mp4', 'https://www.youtube.com/watch?v=f0UB06v7yLY')
+#model.add_video('oren', 'oren_speech_stevens_institute.mp4', 'https://www.youtube.com/watch?v=V2V0Yiy0Afs')
+
+# When getting ready to train
+model.fetch()
+model.preprocess()
+
+# Interactive for now
+model.train()
 
 
-
+# Some day later
+#model.add_video('oren', 'input/oren/oren.mp4') # Existing video file
+#model.add_images('oren', 'input/oren_images') # Folder of images
