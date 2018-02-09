f2744c49c5aa557196138450ecf0d01ba263195f
Gaurav Oberoi <goberoi@gmail.com>
2018-02-09 15:27:38 -0800

    Various changes.

--- faceit.py
+++ faceit.py
@@ -8,6 +8,7 @@ import numpy
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
 from moviepy.editor import AudioFileClip
+import shutil
 import sys
 sys.path.append('faceswap')
 
@@ -43,17 +44,17 @@ class FaceIt:
         }
         if not os.path.exists(os.path.join(FaceIt.VIDEO_PATH)):
             os.makedirs(FaceIt.VIDEO_PATH)
-
-        # Magic incantation to let tensorflow use more GPU memory
-        if False:
+        if True:
+            # Magic incantation to let tensorflow use more GPU memory
             import tensorflow as tf
             import keras.backend.tensorflow_backend as K
             config = tf.ConfigProto()
             config.gpu_options.allow_growth = True
             config.gpu_options.visible_device_list="0"
             K.set_session(tf.Session(config=config))
+            
 
-    def add_video(self, person, name, url=None, fps=2):
+    def add_video(self, person, name, url=None, fps=20):
         self._videos[person].append({
             'name' : name,
             'url' : url,
@@ -114,7 +115,7 @@ class FaceIt:
         
         os.makedirs(video_frames_dir)
         frame_num = 0
-        for frame in tqdm.tqdm(video_clip.iter_frames(fps=video['fps'])):
+        for frame in tqdm.tqdm(video_clip.iter_frames(fps=video['fps']), total = video_clip.fps * video_clip.duration):
             video_frame_file = os.path.join(video_frames_dir, 'frame_{:03d}.jpg'.format(frame_num))
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV
             cv2.imwrite(video_frame_file, frame)
@@ -136,28 +137,35 @@ class FaceIt:
         os.makedirs(video_faces_dir)
         self._faceswap.extract(self._video_frames_path(video), video_faces_dir, self._faces[person])
 
+    def preprocess(self):
+        self.fetch()
+        self.extract_frames()
+        self.extract_faces()
     
     def _symlink_faces_for_model(self, person, video):
-        if not os.path.exists(self._model_person_data_path(person)):
-            os.makedirs(self._model_person_data_path(person))
         for face_file in os.listdir(self._video_faces_path(video)):
             target_file = os.path.join(self._model_person_data_path(person), video['name'] + "_" + face_file)
             face_file_path = os.path.join(os.getcwd(), self._video_faces_path(video), face_file)
-            print("face_file_path {}, target_file {}".format(face_file_path, target_file))
             os.symlink(face_file_path, target_file)
 
     def train(self):
+        print(self._videos)
+        
         # Setup directory structure for model, and create one director for person_a faces, and
         # another for person_b faces containing symlinks to all faces.
         if not os.path.exists(self._model_path()):
             os.makedirs(self._model_path())
 
-        if not os.path.exists(self._model_data_path()):
-            self._process_videos(self._symlink_faces_for_model)
+        if os.path.exists(self._model_data_path()):
+            shutil.rmtree(self._model_data_path())
+
+        os.makedirs(self._model_person_data_path(self._person_a))
+        os.makedirs(self._model_person_data_path(self._person_b))            
+        self._process_videos(self._symlink_faces_for_model)
 
         self._faceswap.train(self._model_person_data_path(self._person_a), self._model_person_data_path(self._person_b), self._model_path())
 
-    def convert(self, video_file, swap_model = False):
+    def convert(self, video_file, swap_model = False, max_frames = None):
         video_path = self._video_path({ 'name' : video_file })
         video = VideoFileClip(video_path)
         
@@ -167,7 +175,7 @@ class FaceIt:
             exit(1)
 
         converter = PluginLoader.get_converter("Masked")
-        converter = converter(model.converter(swap_model),
+        converter = converter(model.converter(False),
                               blur_size=8,
                               seamless_clone=True,
                               mask_type="facehullandrect",
@@ -177,14 +185,19 @@ class FaceIt:
 
         def _convert_helper(get_frame, t):
             return _convert_frame(get_frame(t))
-        
+
+        frames_converted = 0
         def _convert_frame(frame):
+            nonlocal frames_converted
+            if max_frames and frames_converted > max_frames:
+                return frame
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV            
             for face in detect_faces(frame, "cnn"):
                 # TODO: add filter by face. See self.filter.check(face) in cli.py 
                 frame = converter.patch_image(frame, face)
                 frame = frame.astype(numpy.float32)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV                            
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV
+            frames_converted += 1            
             return frame
 
         # Convert frames one by one
@@ -198,35 +211,33 @@ class FaceIt:
 #        audio = AudioFileClip(video_path)
 #        new_video = new_video.set_audio(audio)
         
-        new_video.write_videofile(video_file)
+        new_video.write_videofile(video_file, rewrite_audio = True)
         del video
         del new_video                
 
 class FaceSwapInterface:
     def __init__(self):
         self._parser = FullHelpArgumentParser()
-        subparser = self._parser.add_subparsers()
-        extract = ExtractTrainingData(
-            subparser, "extract", "Extract the faces from a pictures.")
-        train = TrainingProcessor(
-            subparser, "train", "This command trains the model for the two faces A and B.")
-        convert = ConvertImage(
-            subparser, "convert", "Convert a source image to a new one with the face swapped.")
+        self._subparser = self._parser.add_subparsers()
 
     def extract(self, input_dir, output_dir, filter_path):
+        extract = ExtractTrainingData(
+            self._subparser, "extract", "Extract the faces from a pictures.")
         args_str = "extract --input-dir {} --output-dir {} --filter {} --processes 1 --detector cnn"
         args_str = args_str.format(input_dir, output_dir, filter_path)
         self._run_script(args_str)
 
     def train(self, input_a_dir, input_b_dir, model_dir):
+        train = TrainingProcessor(
+            self._subparser, "train", "This command trains the model for the two faces A and B.")
         args_str = "train --input-A {} --input-B {} --model-dir {} --batch-size {} --write-image"
-        args_str = args_str.format(input_a_dir, input_b_dir, model_dir, 256)
+        args_str = args_str.format(input_a_dir, input_b_dir, model_dir, 512)
         self._run_script(args_str)
 
     def _run_script(self, args_str):
         args = self._parser.parse_args(args_str.split(' '))
         args.func(args)
-        
+
 
 
 
@@ -239,25 +250,37 @@ class FaceSwapInterface:
 
 #faceit = FaceIt('pikotaro_to_jacob', 'pikotaro', 'jacob')
 
-faceit = FaceIt('rick_to_jacob', 'rick', 'jacob')
-faceit.add_video('rick', 'rick_never_gonna_give_you_up.mp4', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+#faceit = FaceIt('rick_to_jacob', 'rick', 'jacob')
+#faceit.add_video('rick', 'rick_never_gonna_give_you_up.mp4', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 20)
+#faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
+#faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
+#faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
+#faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
+
+faceit = FaceIt('bezos_to_jacob', 'bezos', 'jacob')
+faceit.add_video('bezos', 'bezos_alexa_lost_her_voice.mp4', 'https://www.youtube.com/watch?v=YLPNjaAOrBw')
+faceit.add_video('bezos', 'bezos_ice_bucket.mp4', 'https://www.youtube.com/watch?v=DFVezzjAhFY', fps=5)
 faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
 faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
 faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
 faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
-
 
 #faceit = FaceIt('trump_to_oren', 'trump', 'oren')
 #faceit.add_video('trump', 'trump_speech_compilation.mp4', 'https://www.youtube.com/watch?v=f0UB06v7yLY')
 #faceit.add_video('oren', 'oren_speech_stevens_institute.mp4', 'https://www.youtube.com/watch?v=V2V0Yiy0Afs')
 
 # When getting ready to train
-faceit.fetch()
-faceit.extract_frames()
-faceit.extract_faces()
+#faceit.preprocess()
 
 # Interactive for now
-faceit.train()
+#faceit.train()
 
+#faceit.convert('bezos_alexa_lost_her_voice.mp4')
+
+#faceit.convert('bezos_ice_bucket.mp4', max_frames = 300)
+
+faceit.convert('jacob_wall.mp4', max_frames = 300, swap_model = True)
+
+#faceit.convert('rick_never_gonna_give_you_up.mp4')
 
 #faceit.convert('pikotaro_music_video.mp4')
