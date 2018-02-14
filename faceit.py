a2f6d0f07d2c3667de38f1652266717ba67a3d73
Gaurav Oberoi <goberoi@gmail.com>
2018-02-14 15:38:09 -0800

    Tidied up how to run the app.

--- faceit.py
+++ faceit.py
@@ -1,5 +1,6 @@
 import os
 from argparse import Namespace
+import argparse
 import youtube_dl
 import cv2
 import time
@@ -26,37 +27,36 @@ class FaceIt:
     PERSON_PATH = 'data/persons'
     PROCESSED_PATH = 'data/processed'
     MODEL_PATH = 'models'
+    MODELS = {}
+
+    @classmethod
+    def add_model(cls, model):
+        FaceIt.MODELS[model._name] = model
     
     def __init__(self, name, person_a, person_b):
-        self._faceswap = FaceSwapInterface()
-
-        self._name = name
+        def _create_person_data(person):
+            return {
+                'name' : person,
+                'videos' : [],
+                'faces' : os.path.join(FaceIt.PERSON_PATH, person + '.jpg'),
+            }
         
+        self._name = name
+
+        self._people = {
+            person_a : _create_person_data(person_a),
+            person_b : _create_person_data(person_b),
+        }
         self._person_a = person_a
         self._person_b = person_b
+        
+        self._faceswap = FaceSwapInterface()
 
-        self._faces = {
-            self._person_a : os.path.join(FaceIt.PERSON_PATH, person_a + '.jpg'),
-            self._person_b : os.path.join(FaceIt.PERSON_PATH, person_b + '.jpg')
-        }
-        self._videos = {
-            self._person_a : [],
-            self._person_b : []
-        }
         if not os.path.exists(os.path.join(FaceIt.VIDEO_PATH)):
-            os.makedirs(FaceIt.VIDEO_PATH)
-        if True:
-            # Magic incantation to let tensorflow use more GPU memory
-            import tensorflow as tf
-            import keras.backend.tensorflow_backend as K
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            config.gpu_options.visible_device_list="0"
-            K.set_session(tf.Session(config=config))
-            
+            os.makedirs(FaceIt.VIDEO_PATH)            
 
     def add_video(self, person, name, url=None, fps=20):
-        self._videos[person].append({
+        self._people[person]['videos'].append({
             'name' : name,
             'url' : url,
             'fps' : fps
@@ -69,11 +69,14 @@ class FaceIt:
         self._process_videos(self._extract_frames)
 
     def extract_faces(self):        
-        self._process_videos(self._extract_faces)        
+        self._process_videos(self._extract_faces)
+
+    def all_videos(self):
+        return self._people[self._person_a]['videos'] + self._people[self._person_b]['videos']
 
     def _process_videos(self, func):
-        for person, videos in self._videos.items():
-            for video in videos:
+        for person in self._people:
+            for video in self._people[person]['videos']:
                 func(person, video)
 
     def _video_path(self, video):
@@ -139,7 +142,7 @@ class FaceIt:
             return
         
         os.makedirs(video_faces_dir)
-        self._faceswap.extract(self._video_frames_path(video), video_faces_dir, self._faces[person])
+        self._faceswap.extract(self._video_frames_path(video), video_faces_dir, self._people[person]['faces'])
 
     def preprocess(self):
         self.fetch()
@@ -161,13 +164,21 @@ class FaceIt:
         if os.path.exists(self._model_data_path()):
             shutil.rmtree(self._model_data_path())
 
-        os.makedirs(self._model_person_data_path(self._person_a))
-        os.makedirs(self._model_person_data_path(self._person_b))            
+        for person in self._people:
+            os.makedirs(self._model_person_data_path(person))
         self._process_videos(self._symlink_faces_for_model)
 
         self._faceswap.train(self._model_person_data_path(self._person_a), self._model_person_data_path(self._person_b), self._model_path(use_gan), use_gan)
 
     def convert(self, video_file, swap_model = False, max_frames = None, use_gan = False):
+        # Magic incantation to let tensorflow use more GPU memory
+        import tensorflow as tf
+        import keras.backend.tensorflow_backend as K
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        config.gpu_options.visible_device_list="0"
+        K.set_session(tf.Session(config=config))
+        
         video_path = self._video_path({ 'name' : video_file })
         video = VideoFileClip(video_path)
 
@@ -195,7 +206,7 @@ class FaceIt:
         filter_person = self._person_a
         if swap_model:
             filter_person = self._person_b
-        filter = FaceFilter(self._faces[filter_person])
+        filter = FaceFilter(self._people[filter_person]['faces'])
 
         def _convert_helper(get_frame, t):
             return _convert_frame(get_frame(t))
@@ -248,51 +259,46 @@ class FaceSwapInterface:
         args.func(args)
 
 
+if __name__ == '__main__':
+    
+    faceit = FaceIt('rick_to_jacob', 'rick', 'jacob')
+    faceit.add_video('rick', 'rick_never_gonna_give_you_up.mp4', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 20)
+    faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
+    faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
+    faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
+    faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
+    FaceIt.add_model(faceit)
+    
+    faceit = FaceIt('bezos_to_jacob', 'bezos', 'jacob')
+    faceit.add_video('bezos', 'bezos_alexa_lost_her_voice.mp4', 'https://www.youtube.com/watch?v=YLPNjaAOrBw')
+    faceit.add_video('bezos', 'bezos_ice_bucket.mp4', 'https://www.youtube.com/watch?v=DFVezzjAhFY', fps=5)
+    faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
+    faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
+    faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
+    faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
+    FaceIt.add_model(faceit)
+    
+    faceit = FaceIt('trump_to_oren', 'trump', 'oren')
+    faceit.add_video('trump', 'trump_speech_compilation.mp4', 'https://www.youtube.com/watch?v=f0UB06v7yLY')
+    faceit.add_video('oren', 'oren_speech_stevens_institute.mp4', 'https://www.youtube.com/watch?v=V2V0Yiy0Afs')
+    faceit.add_video('oren', 'oren_future_of_data_mining.mp4', 'https://www.youtube.com/watch?v=ZCsrUI9kGII', fps=5)
+    FaceIt.add_model(faceit)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('task', choices = ['train', 'convert'])
+    parser.add_argument('model', choices = FaceIt.MODELS.keys())
+    parser.add_argument('video', nargs = '?')
+    parser.add_argument('--max-frames')
+    parser.add_argument('--swap-model', action = 'store_true')
+    args = parser.parse_args()
 
-#faceit = FaceIt('pikotaro_to_jacob', 'pikotaro', 'jacob')
-#faceit.add_video('pikotaro', 'pikotaro_music_video.mp4', 'https://www.youtube.com/watch?v=Ct6BUPvE2sM', fps=20)
-#faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
-#faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
-#faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
-#faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
+    faceit = FaceIt.MODELS[args.model]
 
-#faceit = FaceIt('pikotaro_to_jacob', 'pikotaro', 'jacob')
-
-#faceit = FaceIt('rick_to_jacob', 'rick', 'jacob')
-#faceit.add_video('rick', 'rick_never_gonna_give_you_up.mp4', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 20)
-#faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
-#faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
-#faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
-#faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
-
-#faceit = FaceIt('bezos_to_jacob', 'bezos', 'jacob')
-#faceit.add_video('bezos', 'bezos_alexa_lost_her_voice.mp4', 'https://www.youtube.com/watch?v=YLPNjaAOrBw')
-#faceit.add_video('bezos', 'bezos_ice_bucket.mp4', 'https://www.youtube.com/watch?v=DFVezzjAhFY', fps=5)
-#faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
-#faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
-#faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
-#faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
-
-faceit = FaceIt('trump_to_oren', 'trump', 'oren')
-faceit.add_video('trump', 'trump_speech_compilation.mp4', 'https://www.youtube.com/watch?v=f0UB06v7yLY')
-faceit.add_video('oren', 'oren_speech_stevens_institute.mp4', 'https://www.youtube.com/watch?v=V2V0Yiy0Afs')
-faceit.add_video('oren', 'oren_future_of_data_mining.mp4', 'https://www.youtube.com/watch?v=ZCsrUI9kGII', fps=5)
-
-# When getting ready to train
-faceit.preprocess()
-
-# Interactive for now
-#faceit.train(use_gan = False)
-
-faceit.convert('trump_speech_compilation.mp4')
-
-#faceit.convert('bezos_alexa_lost_her_voice.mp4', max_frames = 30000, use_gan = False)
-
-#faceit.convert('bezos_ice_bucket.mp4', max_frames = 600, use_gan = False)
-
-#faceit.convert('jacob_wall.mp4', max_frames = 300, swap_model = True, use_gan = True)
-
-#faceit.convert('rick_never_gonna_give_you_up.mp4', swap_model = True, max_frames = 600)
-
-#faceit.convert('pikotaro_music_video.mp4')
+    if args.task == 'train':
+        faceit.preprocess()
+        faceit.train()
+    elif args.task == 'convert':
+        if not args.video:
+            print('Need a video to convert. Some ideas: {}'.format(", ".join([video['name'] for video in faceit.all_videos()])))
+        else:
+            faceit.convert(args.video, max_frames = args.max_frames, swap_model = args.swap_model)
