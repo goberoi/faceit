e0c59f0f07811544f764c67209c62a208302b911
Gaurav Oberoi <goberoi@gmail.com>
2018-02-14 14:04:12 -0800

    Added option to train with GAN model.

--- faceit.py
+++ faceit.py
@@ -19,7 +19,7 @@ from scripts.train import TrainingProcessor
 from scripts.convert import ConvertImage
 from lib.faces_detect import detect_faces
 from plugins.PluginLoader import PluginLoader
-
+from lib.FaceFilter import FaceFilter
 
 class FaceIt:
     VIDEO_PATH = 'data/videos'
@@ -85,8 +85,11 @@ class FaceIt:
     def _video_faces_path(self, video):
         return os.path.join(FaceIt.PROCESSED_PATH, video['name'] + '_faces')
 
-    def _model_path(self):
-        return os.path.join(FaceIt.MODEL_PATH, self._name)
+    def _model_path(self, use_gan = False):
+        path = FaceIt.MODEL_PATH
+        if use_gan:
+            path += "_gan"
+        return os.path.join(path, self._name)
 
     def _model_data_path(self):
         return os.path.join(FaceIt.PROCESSED_PATH, "model_data_" + self._name)
@@ -149,11 +152,11 @@ class FaceIt:
             face_file_path = os.path.join(os.getcwd(), self._video_faces_path(video), face_file)
             os.symlink(face_file_path, target_file)
 
-    def train(self):
+    def train(self, use_gan = False):
         # Setup directory structure for model, and create one director for person_a faces, and
         # another for person_b faces containing symlinks to all faces.
-        if not os.path.exists(self._model_path()):
-            os.makedirs(self._model_path())
+        if not os.path.exists(self._model_path(use_gan)):
+            os.makedirs(self._model_path(use_gan))
 
         if os.path.exists(self._model_data_path()):
             shutil.rmtree(self._model_data_path())
@@ -162,20 +165,25 @@ class FaceIt:
         os.makedirs(self._model_person_data_path(self._person_b))            
         self._process_videos(self._symlink_faces_for_model)
 
-        self._faceswap.train(self._model_person_data_path(self._person_a), self._model_person_data_path(self._person_b), self._model_path())
+        self._faceswap.train(self._model_person_data_path(self._person_a), self._model_person_data_path(self._person_b), self._model_path(use_gan), use_gan)
 
-    def convert(self, video_file, swap_model = False, max_frames = None):
+    def convert(self, video_file, swap_model = False, max_frames = None, use_gan = False):
         video_path = self._video_path({ 'name' : video_file })
         video = VideoFileClip(video_path)
-        
-        model = PluginLoader.get_model("Original")(Path(self._model_path()))
+
+        model_name = "Original"
+        converter_name = "Masked"
+        if use_gan:
+            model_name = "GAN"
+            converter_name = "GAN"
+        model = PluginLoader.get_model(model_name)(Path(self._model_path(use_gan)))
 
         if not model.load(swap_model):
             print('model Not Found! A valid model must be provided to continue!')
             exit(1)
 
 
-        converter = PluginLoader.get_converter("Masked")
+        converter = PluginLoader.get_converter(converter_name)
         converter = converter(model.converter(False),
                               blur_size=8,
                               seamless_clone=True,
@@ -183,6 +191,11 @@ class FaceIt:
                               erosion_kernel_size=None,
                               smooth_mask=True,
                               avg_color_adjust=True)
+
+        filter_person = self._person_a
+        if swap_model:
+            filter_person = self._person_b
+        filter = FaceFilter(self._faces[filter_person])
 
         def _convert_helper(get_frame, t):
             return _convert_frame(get_frame(t))
@@ -194,9 +207,9 @@ class FaceIt:
                 return frame
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV            
             for face in detect_faces(frame, "cnn"):
-                # TODO: add filter by face. See self.filter.check(face) in cli.py 
-                frame = converter.patch_image(frame, face)
-                frame = frame.astype(numpy.float32)
+                if filter.check(face):
+                    frame = converter.patch_image(frame, face)
+                    frame = frame.astype(numpy.float32)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Swap RGB to BGR to work with OpenCV
             frames_converted += 1            
             return frame
@@ -220,11 +233,14 @@ class FaceSwapInterface:
         args_str = args_str.format(input_dir, output_dir, filter_path)
         self._run_script(args_str)
 
-    def train(self, input_a_dir, input_b_dir, model_dir):
+    def train(self, input_a_dir, input_b_dir, model_dir, gan = False):
+        model_type = "Original"
+        if gan:
+            model_type = "GAN"
         train = TrainingProcessor(
             self._subparser, "train", "This command trains the model for the two faces A and B.")
-        args_str = "train --input-A {} --input-B {} --model-dir {} --batch-size {} --write-image"
-        args_str = args_str.format(input_a_dir, input_b_dir, model_dir, 512)
+        args_str = "train --input-A {} --input-B {} --model-dir {} --trainer {} --batch-size {} --write-image"
+        args_str = args_str.format(input_a_dir, input_b_dir, model_dir, model_type, 512)
         self._run_script(args_str)
 
     def _run_script(self, args_str):
@@ -250,30 +266,33 @@ class FaceSwapInterface:
 #faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
 #faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
 
-faceit = FaceIt('bezos_to_jacob', 'bezos', 'jacob')
-faceit.add_video('bezos', 'bezos_alexa_lost_her_voice.mp4', 'https://www.youtube.com/watch?v=YLPNjaAOrBw')
-faceit.add_video('bezos', 'bezos_ice_bucket.mp4', 'https://www.youtube.com/watch?v=DFVezzjAhFY', fps=5)
-faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
-faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
-faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
-faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
+#faceit = FaceIt('bezos_to_jacob', 'bezos', 'jacob')
+#faceit.add_video('bezos', 'bezos_alexa_lost_her_voice.mp4', 'https://www.youtube.com/watch?v=YLPNjaAOrBw')
+#faceit.add_video('bezos', 'bezos_ice_bucket.mp4', 'https://www.youtube.com/watch?v=DFVezzjAhFY', fps=5)
+#faceit.add_video('jacob', 'jacob_rolex.mp4', 'https://www.youtube.com/watch?v=HPcbjLJXelU')
+#faceit.add_video('jacob', 'jacob_wall.mp4', 'https://www.youtube.com/watch?v=91LULLWBRqk')
+#faceit.add_video('jacob', 'jacob_pitch.mp4', 'https://www.youtube.com/watch?v=smRCM5Smwls')
+#faceit.add_video('jacob', 'jacob_interview.mp4', 'https://www.youtube.com/watch?v=Y-mYHCO9lF8')
 
-#faceit = FaceIt('trump_to_oren', 'trump', 'oren')
-#faceit.add_video('trump', 'trump_speech_compilation.mp4', 'https://www.youtube.com/watch?v=f0UB06v7yLY')
-#faceit.add_video('oren', 'oren_speech_stevens_institute.mp4', 'https://www.youtube.com/watch?v=V2V0Yiy0Afs')
+faceit = FaceIt('trump_to_oren', 'trump', 'oren')
+faceit.add_video('trump', 'trump_speech_compilation.mp4', 'https://www.youtube.com/watch?v=f0UB06v7yLY')
+faceit.add_video('oren', 'oren_speech_stevens_institute.mp4', 'https://www.youtube.com/watch?v=V2V0Yiy0Afs')
+faceit.add_video('oren', 'oren_future_of_data_mining.mp4', 'https://www.youtube.com/watch?v=ZCsrUI9kGII', fps=5)
 
 # When getting ready to train
-#faceit.preprocess()
+faceit.preprocess()
 
 # Interactive for now
-#faceit.train()
+#faceit.train(use_gan = False)
 
-#faceit.convert('bezos_alexa_lost_her_voice.mp4')
+faceit.convert('trump_speech_compilation.mp4')
 
-#faceit.convert('bezos_ice_bucket.mp4', max_frames = 300)
+#faceit.convert('bezos_alexa_lost_her_voice.mp4', max_frames = 30000, use_gan = False)
 
-#faceit.convert('jacob_wall.mp4', max_frames = 300, swap_model = True)
+#faceit.convert('bezos_ice_bucket.mp4', max_frames = 600, use_gan = False)
 
-faceit.convert('rick_never_gonna_give_you_up.mp4', swap_model = True, max_frames = 600)
+#faceit.convert('jacob_wall.mp4', max_frames = 300, swap_model = True, use_gan = True)
+
+#faceit.convert('rick_never_gonna_give_you_up.mp4', swap_model = True, max_frames = 600)
 
 #faceit.convert('pikotaro_music_video.mp4')
